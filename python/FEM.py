60348f5a91a5b90d57c5c6af8d5fffa7e608a8ca
mtsodf <mtsodf@gmail.com>
2017-06-17 00:17:44 -0300

    Adicao da criacao da matriz de rigidez

--- python/FEM.py
+++ python/FEM.py
@@ -3,7 +3,9 @@ Estudos de Elementos Finitos.
 Resolucao do laplaciano em 2 dimensoes
 """
 
-import numpy
+import numpy as np
+import numpy.linalg as LA
+import math
 
 
 class FemProblem:
@@ -31,10 +33,18 @@ class FemProblem:
     def calcMatrizRigidez(self):
         self.calcGrausDeLiberdade()
 
+
+        m_rigidez = np.zeros((self._graus, self._graus))
+
         for elem in self.elementos:
-            pass
+            for contribuicao in elem.rigidezContribuicao():
+                m_rigidez[contribuicao[0]] += contribuicao[1]
 
+        return m_rigidez
 
+    def LD(self):
+        for elem in self.elementos:
+             
 
 class No:
 
@@ -64,9 +74,85 @@ class Elemento():
     def qtdNos(self):
         return len(self.nos)
 
+    def pontosIntegracao(self):
+
+        x = math.sqrt(3)/3
+
+        pontos = [(-x,-x), (x,-x), (x,x), (-x,x)]
+        pesos = [0.25, 0.25, 0.25, 0.25]
+
+        return pontos, pesos
+
+    def rigidezContribuicao(self):
+        contribuicoes = []
 
 
+        for i, no1 in enumerate(self.nos):
+            if no1.livre:
+                for j, no2 in enumerate(self.nos):
+                    if no2.livre:
 
+                        integral = 0.0
+
+                        pontos, pesos = self.pontosIntegracao()
+
+                        for nk in range(len(pontos)):
+                            ep, mu = pontos[nk]
+
+                            devGlobal = self.calcDevGlobal(ep, mu)
+
+                            jac = self.calcJac(ep, mu)
+
+                            integral += pesos[nk] * (devGlobal[(0,i)]*devGlobal[(0,j)] + devGlobal[(1,i)]*devGlobal[(1,j)]) * LA.det(jac)
+
+
+                        contribuicoes.append([(no1.matrizPos, no2.matrizPos), integral])
+
+        return contribuicoes
+
+    def calcJac(self, ep, mu):
+        nosCoords = self.coordMatriz()
+        dev = self.calcDevLocal(ep, mu)
+
+        return np.dot(dev, nosCoords)
+
+    def calcDevGlobal(self, ep, mu):
+        return np.dot(self.calcJacInv(ep, mu), self.calcDevLocal(ep, mu))
+
+    def calcJacInv(self, ep, mu):
+        return LA.inv(self.calcJac(ep, mu))
+
+
+    def calcDevLocal(self, ep, mu):
+        d = np.zeros((2, self.qtdNos()))
+        dt = 0.0001
+        for i, phi in enumerate(self.phis()): 
+            d[(0, i)] = (phi(ep+dt, mu) - phi(ep-dt, mu)) / (2*dt)
+            d[(1, i)] = (phi(ep, mu+dt) - phi(ep, mu-dt)) / (2*dt)
+
+        return d
+
+    def coordMatriz(self):
+        nosCoords = np.zeros((self.qtdNos(), 2))
+        for i, no in enumerate(self.nos):
+            nosCoords[(i, 0)] = no.x
+            nosCoords[(i, 1)] = no.y
+
+        return nosCoords
+
+    def phis(self):
+        phi0 = lambda ep, mu: 0.25*(1-ep)*(1-mu)
+        phi1 = lambda ep, mu: 0.25*(1+ep)*(1-mu)
+        phi2 = lambda ep, mu: 0.25*(1+ep)*(1+mu)
+        phi3 = lambda ep, mu: 0.25*(1-ep)*(1+mu)
+        return [phi0, phi1, phi2, phi3]
+
+    def devPhi(self):
+        dev_phi0 = lambda ep, mu: 0.25*(1-ep)*(1-mu)
+        dev_phi1 = lambda ep, mu: 0.25*(1+ep)*(1-mu)
+        dev_phi2 = lambda ep, mu: 0.25*(1+ep)*(1+mu)
+        dev_phi3 = lambda ep, mu: 0.25*(1-ep)*(1+mu)
+        return [phi0, phi1, phi2, phi3]
 
 def criarMalha(N):
     h = 1.0/N
--- python/FEMTest.py
+++ python/FEMTest.py
@@ -1,14 +1,18 @@
 import unittest
 from FEM import *
 
+
 class TestStringMethods(unittest.TestCase):
 
     def test_upper(self):
         elementos, nos = criarMalha(4)
 
+        for elem in elementos:
+            self.assertEqual(elem.qtdNos(), 4)
+
         elem = elementos[10]
 
-        self.assertEqual(elem.qtdNos(), 4)
+
 
         self.assertEqual(elem.nos[0].g_ind, 12)
         self.assertEqual(elem.nos[1].g_ind, 13)
@@ -32,6 +36,30 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual(nos[18].matrizPos, 8)
 
 
+        contribuicoes = elem.rigidezContribuicao()
+
+        self.assertEqual(len(contribuicoes), 16)
+
+        self.assertEqual(contribuicoes[0][0], (4, 4))
+        self.assertEqual(contribuicoes[1][0], (4, 5))
+        self.assertEqual(contribuicoes[2][0], (4, 8))
+        self.assertEqual(contribuicoes[3][0], (4, 7))
+
+
+        D = elem.coordMatriz()
+        self.assertEqual(D[(0,0)], 0.50); self.assertEqual(D[(0,1)], 0.50); 
+        self.assertEqual(D[(1,0)], 0.75); self.assertEqual(D[(1,1)], 0.50);
+        self.assertEqual(D[(2,0)], 0.75); self.assertEqual(D[(2,1)], 0.75);
+        self.assertEqual(D[(3,0)], 0.50); self.assertEqual(D[(3,1)], 0.75);
+
+        print elem.calcJac(-1,-1)
+
+
+        print D
+
+        matriz = fem.calcMatrizRigidez()
+
+        print matriz
 
 
 if __name__ == '__main__':
