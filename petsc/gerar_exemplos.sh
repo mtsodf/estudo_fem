bb287a1463575f1607150dafa034e00798a4d2a4
mtsodf <mtsodf@gmail.com>
2017-07-01 18:30:10 -0300

    Adicionando no script caso 4.

--- petsc/gerar_exemplos.sh
+++ petsc/gerar_exemplos.sh
@@ -1,3 +1,4 @@
 python plot_sol.py ../modelos/exemplo1_nos.txt ../modelos/exemplo1_elementos.txt 1
 python plot_sol.py ../modelos/exemplo2_nos.txt ../modelos/exemplo2_elementos.txt 2
 python plot_sol.py ../modelos/exemplo3_nos.txt ../modelos/exemplo3_elementos.txt 3
+python plot_sol.py ../modelos/exemplo4_nos.txt ../modelos/exemplo4_elementos.txt 4
