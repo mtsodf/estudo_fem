1b74df385444c58c751f75ec522c0b60f1f3c7f5
mtsodf <mtsodf@gmail.com>
2017-07-01 17:33:53 -0300

    Adicao de plot das condicoes de contorno na malha

--- petsc/plot_sol.py
+++ petsc/plot_sol.py
@@ -11,17 +11,36 @@ def ler_nos(filename):
 	nos = []
 	nos.append([])
 	nos.append([])
+	nos_livres = []
+	nos_livres.append([])
+	nos_livres.append([])
+	nos_dirichlet = []
+	nos_dirichlet.append([])
+	nos_dirichlet.append([])
+	nos_neumann = []
+	nos_neumann.append([])
+	nos_neumann.append([])
 	f=open(filename)
 	n = int(f.next())
 	for i in range(n):
-		l = f.next()
-		x,y = [float(x) for x in l.split()[:2]]
-
+		split = f.next().split()
+		x,y = [float(x) for x in split[:2]]
+		tipo = int(split[2])
 		nos[0].append(x)
 		nos[1].append(y)
 
+		if(tipo == 0):
+			nos_livres[0].append(x)
+			nos_livres[1].append(y)
+		elif(tipo == 1):
+			nos_dirichlet[0].append(x)
+			nos_dirichlet[1].append(y)
+		elif(tipo == 2):
+			nos_neumann[0].append(x)
+			nos_neumann[1].append(y)
 
-	return nos
+
+	return nos, nos_livres, nos_dirichlet, nos_neumann
 
 def ler_elementos(filename):
 	elementos = []
@@ -39,11 +58,11 @@ print "Argumentos: ", sys.argv
 if len(sys.argv) > 1:
     nos = sys.argv[1]
     elementos = sys.argv[2]
-    copyfile("%s"%nos, "nos.txt")
-    copyfile("%s"%elementos, "elementos.txt")
-    os.system("./Laplaciano %s" % sys.argv[3])
+    #copyfile("%s"%nos, "nos.txt")
+    #copyfile("%s"%elementos, "elementos.txt")
+    #os.system("./Laplaciano %s" % sys.argv[3])
 
-nos = ler_nos("nos.txt")
+nos, nos_livres, nos_dirichlet, nos_neumann = ler_nos("nos.txt")
 elementos = ler_elementos("elementos.txt")
 
 
@@ -75,8 +94,6 @@ ax.set_zlabel('Z')
 
 ax = fig.add_subplot(122)
 
-ax.scatter(nos[0], nos[1],s=10)
-
 for elem in elementos:
 	
 	xs = []
@@ -89,7 +106,11 @@ for elem in elementos:
 	xs.append(nos[0][elem[0]])
 	ys.append(nos[1][elem[0]])
 	
-	ax.plot(xs, ys, 'k-')
+	ax.plot(xs, ys, 'k-', linewidth=.5)
+
+ax.scatter(nos_livres[0], nos_livres[1], s=10, c="b")
+ax.scatter(nos_dirichlet[0], nos_dirichlet[1], s=10, c="r")
+ax.scatter(nos_neumann[0], nos_neumann[1], s=10, c="y")
 
 
 plt.savefig("exemplo%s.png" % sys.argv[3])
