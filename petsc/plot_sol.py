4ecae48566c03267cb4628c9e09842a9aff6b988
mtsodf <mtsodf@gmail.com>
2017-07-01 14:34:21 -0300

    Adicao de plot do dominio utilizado no problema.

--- petsc/plot_sol.py
+++ petsc/plot_sol.py
@@ -9,23 +9,30 @@ from shutil import copyfile
 
 def ler_nos(filename):
 	nos = []
+	nos.append([])
+	nos.append([])
 	f=open(filename)
 	n = int(f.next())
 	for i in range(n):
 		l = f.next()
-		nos.append([float(x) for x in l.split()[:3]])
+		x,y = [float(x) for x in l.split()[:2]]
+
+		nos[0].append(x)
+		nos[1].append(y)
 
 
+	return nos
 
 def ler_elementos(filename):
-	nos = []
+	elementos = []
 	f=open(filename)
 	n = int(f.next())
 	for i in range(n):
 		l = f.next()
-		nos.append([float(x) for x in l.split()[:3]])
+		elementos.append([int(x) for x in l.split()])
 
 
+	return elementos
 
 print "Argumentos: ", sys.argv
 
@@ -37,7 +44,7 @@ if len(sys.argv) > 1:
     os.system("./Laplaciano %s" % sys.argv[3])
 
 nos = ler_nos("nos.txt")
-#elementos = ler_elementos("elementos.txt")
+elementos = ler_elementos("elementos.txt")
 
 
 f = open("saida.txt")
@@ -58,11 +65,31 @@ z = array(z)
 
 
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure(figsize=(30,15))
+ax = fig.add_subplot(121, projection='3d')
 #ax = axarr[0].Axes3D(fig)
 ax.scatter3D(ravel(x),ravel(y),ravel(z))
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
 ax.set_zlabel('Z')
+
+ax = fig.add_subplot(122)
+
+ax.scatter(nos[0], nos[1],s=10)
+
+for elem in elementos:
+	
+	xs = []
+	ys = []
+	
+	for i in range(len(elem)):
+		xs.append(nos[0][elem[i]])
+		ys.append(nos[1][elem[i]])
+	
+	xs.append(nos[0][elem[0]])
+	ys.append(nos[1][elem[0]])
+	
+	ax.plot(xs, ys, 'k-')
+
+
 plt.savefig("exemplo%s.png" % sys.argv[3])
